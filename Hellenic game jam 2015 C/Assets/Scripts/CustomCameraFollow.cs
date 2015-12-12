4690a9c197d953725657a84b9fade7f9beea3fcf
AntonDan <anton.dan@hotmail.com>
2015-12-12 04:31:27 +0200

    Project updated

---
+++ Hellenic game jam 2015 C/Assets/Scripts/CustomCameraFollow.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CustomCameraFollow : MonoBehaviour {
+
+    public Transform target;
+    public GameObject border;
+
+    float damping = 0f;
+    float lookAheadFactor = 0f;
+    float lookAheadReturnSpeed = 0f;
+    float lookAheadMoveThreshold = 0f;
+    float yPosRestriction = -1f;
+
+    // script defined borders, defined by public reference from collider
+    Collider2D borderCollider;
+    float minX = -20f;
+    float maxX = 20f;
+    float minY = -20f;
+    float maxY = 20f;
+
+    float offsetZ;
+    Vector3 lastTargetPosition;
+    Vector3 currentVelocity;
+    Vector3 lookAheadPos;
+
+    float orthographicWidth;
+    float orthographicHeight; 
+
+    float nextTimeToSearch = 0;
+
+    // Use this for initialization
+    void Start() {
+        borderCollider = border.GetComponent<Collider2D>();
+        minX = border.transform.position.x - (borderCollider.bounds.extents.x);
+        maxX = border.transform.position.x + (borderCollider.bounds.extents.x);
+        minY = border.transform.position.y - (borderCollider.bounds.extents.y);
+        maxY = border.transform.position.y + (borderCollider.bounds.extents.y);
+
+        lastTargetPosition = target.position;
+        offsetZ = (transform.position - target.position).z;
+        orthographicWidth = GetComponent<Camera>().orthographicSize * ((float) Screen.width / Screen.height);
+        orthographicHeight = GetComponent<Camera>().orthographicSize;
+        transform.parent = null;
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if (target == null) {
+            FindPlayer();
+            return;
+        }
+
+        // only update lookahead pos if accelerating or changed direction
+        float xMoveDelta = (target.position - lastTargetPosition).x;
+
+        bool updateLookAheadTarget = Mathf.Abs(xMoveDelta) > lookAheadMoveThreshold;
+
+        if (updateLookAheadTarget) {
+            lookAheadPos = lookAheadFactor * Vector3.right * Mathf.Sign(xMoveDelta);
+        } else {
+            lookAheadPos = Vector3.MoveTowards(lookAheadPos, Vector3.zero, Time.deltaTime * lookAheadReturnSpeed);
+        }
+
+        Vector3 aheadTargetPos = target.position + lookAheadPos + Vector3.forward * offsetZ;
+        Vector3 newPos = Vector3.SmoothDamp(transform.position, aheadTargetPos, ref currentVelocity, damping);
+
+        newPos = new Vector3(newPos.x, Mathf.Clamp(newPos.y, yPosRestriction, Mathf.Infinity), newPos.z);
+
+        transform.position = newPos;
+
+        if (transform.position.x < (minX + orthographicWidth)) {
+            transform.position = new Vector3(minX + orthographicWidth, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > (maxX - orthographicWidth)) {
+            transform.position = new Vector3(maxX - orthographicWidth, transform.position.y, transform.position.z);
+        }
+        if (transform.position.y < (minY + orthographicHeight)) {
+            transform.position = new Vector3(transform.position.x, minY + orthographicHeight, transform.position.z);
+        }
+        if (transform.position.y > (maxY - orthographicHeight)) {
+            transform.position = new Vector3(transform.position.x, maxY - orthographicHeight, transform.position.z);
+        }
+
+        lastTargetPosition = target.position;
+    }
+ 
+    void FindPlayer() {
+        if (nextTimeToSearch <= Time.time) {
+            GameObject searchResult = GameObject.FindGameObjectWithTag("Player");
+            if (searchResult != null)
+                target = searchResult.transform;
+            nextTimeToSearch = Time.time + 0.5f;
+        }
+    }
+}
---
+++ Hellenic game jam 2015 C/Assets/Scripts/PatrollerEnemyScript.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PatrollerEnemyScript : MonoBehaviour {
+    public int healthPoints = 5;
+    public float movementSpeed = 5f;
+    [Range(-1,1)]
+    public int direction = -1;
+    public Vector3 leftmostPosition;
+    public Vector3 rightmostPosition;
+
+    public float timeToWaitAfterLoosingPlayer = 1f;
+    public float timeToKeepCharging = 0.5f;
+
+    float initialSpeed;
+    bool charging = false;
+    bool spottedPlayer = false;
+
+    Vector3 startingPosition;
+    Vector3 targetPosition;
+
+    void Start() {
+        initialSpeed = movementSpeed;
+    }
+	
+	void Update () {
+        if (!charging) {
+            transform.Translate(Vector3.right * direction * movementSpeed * Time.deltaTime);
+        } else {
+            if (targetPosition.x < transform.position.x) direction = -1;
+            else direction = 1;
+            transform.Translate(Vector3.right * direction * movementSpeed * Time.deltaTime);
+        }
+    }
+
+    void LateUpdate() {
+        if (!charging) {
+            if ((transform.position.x >= rightmostPosition.x && direction > 0) || (transform.position.x <= leftmostPosition.x && direction < 0)) {
+                direction = -direction;
+                GetComponentInChildren<BoxCollider2D>().offset = new Vector2(-GetComponentInChildren<BoxCollider2D>().offset.x, GetComponentInChildren<BoxCollider2D>().offset.y);
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Player") {
+            targetPosition = other.transform.position;
+            StopCoroutine(TargetLost());
+            spottedPlayer = true;
+            charging = true;
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D other) {
+        if (other.tag == "Player") {
+            if (!charging) charging = !charging;
+            if (charging) {
+                targetPosition = other.transform.position;
+                if (movementSpeed <= initialSpeed)
+                    movementSpeed = 2 * movementSpeed;
+            } else {
+                movementSpeed = initialSpeed;
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other) {
+        if (other.tag == "Player") {
+            spottedPlayer = false;
+            StartCoroutine(TargetLost());
+        }
+    }
+
+    void Damage() {
+        healthPoints--;
+        if (healthPoints == 0) {
+            Death(0f);
+        }
+    }
+
+    void Death(float deathTime) {
+        Destroy(gameObject, deathTime);
+    }
+
+    IEnumerator TargetLost() {
+        yield return new WaitForSeconds(timeToWaitAfterLoosingPlayer);
+        if (!spottedPlayer) {
+            charging = false;
+            movementSpeed = initialSpeed;
+        }
+        yield return null;
+    }
+
+    IEnumerator ChargeTimer() {
+        while (charging) {
+            yield return new WaitForSeconds(timeToKeepCharging);
+        }
+        yield return null;
+    }
+}
